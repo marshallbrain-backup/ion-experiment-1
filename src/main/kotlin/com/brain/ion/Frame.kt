a91e595baefe6423079eef789a75d800024cfa1b
marshallbrain <marshalldbrain@gmail.com>
2019-09-28 19:20:04 -0400

    Moved JFrame initiation to separate class

--- src/main/kotlin/com/brain/ion/Frame.kt
+++ src/main/kotlin/com/brain/ion/Frame.kt
@@ -1,2 +1,35 @@
 package com.brain.ion
 
+import java.awt.Canvas
+import java.awt.Dimension
+import java.awt.Point
+import javax.swing.JFrame
+
+class Frame(
+	name: String = "Ion",
+	x: Int = 0,
+	y: Int = 0,
+	width: Int = 0,
+	height: Int = 0,
+	decorated: Boolean = true
+) {
+	
+	private val mainFrame = JFrame();
+	private val canvas = Canvas()
+	
+	init {
+		
+		mainFrame.title = "Pulsar";
+		mainFrame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE;
+		mainFrame.isResizable = false;
+		mainFrame.isVisible = true;
+		mainFrame.location = Point(x, y)
+		
+		canvas.preferredSize = Dimension(width, height)
+		mainFrame.add(canvas)
+		
+		mainFrame.pack();
+		
+	}
+	
+}
--- src/main/kotlin/com/brain/ion/Ion.kt
+++ src/main/kotlin/com/brain/ion/Ion.kt
@@ -1,24 +1,10 @@
 package com.brain.ion
 
-import java.awt.Canvas
-import javax.swing.JFrame
-
 class Ion () {
 
 	init {
-
-		var mainFrame = JFrame();
-
-		mainFrame.title = "Pulsar";
-		mainFrame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE;
-		mainFrame.isResizable = false;
-		mainFrame.isVisible = true;
-
-		var canvas = Canvas()
-		canvas.setBounds(50, 50, 1600, 900)
-		mainFrame.add(canvas)
-
-		mainFrame.pack();
+		
+		Frame(x = 50, y = 50, width = 1600, height = 900)
 
 	}
 
