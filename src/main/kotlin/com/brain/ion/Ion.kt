1ad91889f5e90c8922de7a212f69d3545bec11f5
marshallbrain <marshalldbrain@gmail.com>
2019-09-29 10:39:01 -0400

    Actually starts the game loop

--- src/main/kotlin/com/brain/ion/Ion.kt
+++ src/main/kotlin/com/brain/ion/Ion.kt
@@ -4,6 +4,6 @@ class Ion (name: String = "ion") {
 	
 	val mainFrame = Frame(name = name, x = 50, y = 50, width = 1600, height = 900)
 	val minorFrames = mutableListOf<Frame>()
-	val gameLoop = GameLoop(mainFrame, minorFrames)
+	val gameLoop = GameLoop(mainFrame, minorFrames).start()
 
 }
