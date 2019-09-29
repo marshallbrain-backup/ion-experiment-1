2f71ec40eb1cc29907376c5c5cbf247121a3b894
marshallbrain <marshalldbrain@gmail.com>
2019-09-28 21:54:26 -0400

    Created values
    Created the following values
    - mainFrame, the main frame for the program. Closing this frame closes the program.
    - minorFrames, secondary frames for the program. Closing these frames does not close the program.
    - gameLoop, the instance of the current game loop for the program.

--- src/main/kotlin/com/brain/ion/Ion.kt
+++ src/main/kotlin/com/brain/ion/Ion.kt
@@ -1,11 +1,9 @@
 package com.brain.ion
 
-class Ion () {
-
-	init {
-		
-		Frame(x = 50, y = 50, width = 1600, height = 900)
-
-	}
+class Ion (name: String = "ion") {
+	
+	val mainFrame = Frame(name = name, x = 50, y = 50, width = 1600, height = 900)
+	val minorFrames = mutableListOf<Frame>()
+	val gameLoop = GameLoop(mainFrame, minorFrames)
 
 }
