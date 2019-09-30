ad70bc9ddc065d2444021e710bd944eb66228bfc
marshallbrain <marshalldbrain@gmail.com>
2019-09-30 12:07:38 -0400

    Implement getShape

--- src/main/kotlin/com/brain/ion/components/vectors/Rectangle.kt
+++ src/main/kotlin/com/brain/ion/components/vectors/Rectangle.kt
@@ -1,5 +1,7 @@
 package com.brain.ion.components.vectors
 
+import java.awt.Rectangle
+import java.awt.Shape
 
 class Rectangle(
 	private val x: Int = 0,
@@ -8,9 +10,9 @@ class Rectangle(
 	private val height: Int = 0,
 	val style: Style = Style()
 ): Vector {
-	
-	override fun getShape() {
-		TODO("not implemented")
+
+	override fun getShape(): Shape {
+		return Rectangle(x, y, width, height)
 	}
-	
+
 }
