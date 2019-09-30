4545921573ba565d9fc21e04a1697d69c0a5a967
marshallbrain <marshalldbrain@gmail.com>
2019-09-29 20:18:50 -0400

    Initialize the class

--- src/main/kotlin/com/brain/ion/components/vectors/Rectangle.kt
+++ src/main/kotlin/com/brain/ion/components/vectors/Rectangle.kt
@@ -1,4 +1,17 @@
 package com.brain.ion.components.vectors
 
-class Rectangle: Vector {
+import com.brain.ion.components.Style
+
+class Rectangle(
+	val x: Int = 0,
+	val y: Int = 0,
+	val width: Int = 0,
+	val height: Int = 0,
+	val style: Style = Style()
+): Vector {
+	
+	override fun getShape() {
+		TODO("not implemented")
+	}
+	
 }
