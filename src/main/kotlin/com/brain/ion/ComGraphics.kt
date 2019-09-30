26930b380eb689050c18ae5e79ac68d931f17c6a
marshallbrain <marshalldbrain@gmail.com>
2019-09-29 20:23:22 -0400

    Initialize the class
    - Sets a background
    - Sets a clip
    - Allows initialization with either Graphics or Graphics2D

--- src/main/kotlin/com/brain/ion/ComGraphics.kt
+++ src/main/kotlin/com/brain/ion/ComGraphics.kt
@@ -1,4 +1,25 @@
 package com.brain.ion
 
-class ComGraphics {
+import java.awt.Color
+import java.awt.Graphics
+import java.awt.Graphics2D
+import java.awt.Rectangle
+
+class ComGraphics(
+	val graphics: Graphics2D,
+	bounds: Rectangle
+) {
+	
+	init {
+		graphics.color = Color.DARK_GRAY
+		graphics.setClip(bounds.x, bounds.y, bounds.width, bounds.height)
+		graphics.fillRect(bounds.x, bounds.y, bounds.width, bounds.height)
+	}
+	
+	constructor(g: Graphics, bounds: Rectangle) : this(g as Graphics2D, bounds)
+	
+	fun render() {
+		TODO("not implemented")
+	}
+	
 }
