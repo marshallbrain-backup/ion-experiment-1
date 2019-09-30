06db416e7b38eb02d05bc781a699fe0657d6d1bf
marshallbrain <marshalldbrain@gmail.com>
2019-09-30 12:04:17 -0400

    Move style class

--- src/main/kotlin/com/brain/ion/components/vectors/Vector.kt
+++ src/main/kotlin/com/brain/ion/components/vectors/Vector.kt
@@ -1,7 +1,23 @@
 package com.brain.ion.components.vectors
 
+import java.awt.Shape
+
 interface Vector {
 	
-	public fun getShape()
+	fun getShape(): Shape
 	
+}
+
+class Style(
+		strokeColorUnchecked: String = "#FFFFFF",
+		fillColorUnchecked: String = "#FFFFFF",
+		val strokeOpacity: Float = 0f,
+		val fillOpacity: Float = 0f,
+		val strokeWidth: Int = 1
+) {
+
+	val strokeColor = strokeColorUnchecked.removePrefix("#")
+	val fillColor = fillColorUnchecked.removePrefix("#")
+	//g.color = Color.decode("#FFFF00")
+
 }
