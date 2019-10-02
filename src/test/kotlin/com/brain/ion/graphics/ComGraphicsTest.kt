9f31ebe0b772ca3a0e214b1060c972b180e469b8
marshallbrain <marshalldbrain@gmail.com>
2019-10-01 23:28:54 -0400

    Change the structurer of the internal classes
    Added test for the creation of creating render groups.

--- src/test/kotlin/com/brain/ion/graphics/ComGraphicsTest.kt
+++ src/test/kotlin/com/brain/ion/graphics/ComGraphicsTest.kt
@@ -1,25 +1,60 @@
 package com.brain.ion.graphics
 
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
+
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Nested
+import java.awt.Rectangle
 
 internal class ComGraphicsTest {
-    
-    @Nested
-    internal class CreateRenderGroup {
-        @Test
-        fun `group is created`() {}
-        @Test
-        fun `id already exist`() {}
-    }
-    
-    @Nested
-    internal class AddRenderGroup {
-        @Test
-        fun `group is added`() {}
-        @Test
-        fun `id already exist`() {}
-    }
-    
+	
+	private val graphics = ComGraphics(Rectangle(0, 0))
+	
+	@Nested
+	inner class CreateRenderGroup {
+		@Test
+		fun `group is created`() {
+			graphics.createRenderGroup("test")
+			
+			assertThat(graphics.renderGroups).containsKey("test")
+			assertThat(graphics.renderGroups).containsValue(ComGraphics.Group("test"))
+		}
+		
+		@Test
+		fun `id already exist`() {
+		}
+	}
+	
+	@Nested
+	inner class RemoveRenderGroup {
+		@Test
+		fun `group is removed using id`() {
+		}
+		
+		@Test
+		fun `id already exist`() {
+		}
+	}
+	
+	@Nested
+	inner class AddToQueue {
+		@Test
+		fun `vector is added`() {
+		}
+		
+		@Test
+		fun `id already exist`() {
+		}
+	}
+	
+	@Nested
+	inner class RemoveFromQueue {
+		@Test
+		fun `vector is removed`() {
+		}
+		
+		@Test
+		fun `id already exist`() {
+		}
+	}
 }
