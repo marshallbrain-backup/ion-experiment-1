c72e7dbb8edf65c213a16bcf1121da3d6b95d9a9
marshallbrain <marshalldbrain@gmail.com>
2019-10-03 09:48:41 -0400

    Make values private

--- src/main/kotlin/com/brain/ion/Ion.kt
+++ src/main/kotlin/com/brain/ion/Ion.kt
@@ -2,8 +2,8 @@ package com.brain.ion
 
 class Ion (name: String = "ion") {
 	
-	val mainFrame = Frame(name = name, x = 50, y = 50, width = 1600, height = 900)
-	val minorFrames = mutableListOf<Frame>()
-	val gameLoop = GameLoop(mainFrame, minorFrames).start()
+	private val mainFrame = Frame(name = name, x = 50, y = 50, width = 1600, height = 900)
+	private val minorFrames = mutableListOf<Frame>()
+	private val gameLoop = GameLoop(mainFrame, minorFrames)
 
 }
