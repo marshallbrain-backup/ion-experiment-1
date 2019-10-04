b28b889cf9815bafb8e18b24e2beb069d1cb46c4
marshallbrain <marshalldbrain@gmail.com>
2019-10-04 15:59:45 -0400

    Implement unit test for circle class

--- src/test/kotlin/com/brain/ion/components/vectors/CircleTest.kt
+++ src/test/kotlin/com/brain/ion/components/vectors/CircleTest.kt
@@ -1,5 +1,60 @@
 package com.brain.ion.components.vectors
 
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import java.awt.geom.Ellipse2D
+import java.awt.geom.Rectangle2D
 
-internal class CircleTest
+internal class CircleTest {
+	
+	val i = 1
+	val l = 10_000_000_001
+	val f = 1.5f
+	val d = 1.500_000_000_005
+	
+	@Nested
+	inner class CircleCreation() {
+		@Test
+		fun `using ints`() {
+			val rec = Circle(i, i, i)
+			val actual = rec.shape
+			val expected = Ellipse2D.Double(i.toDouble(), i.toDouble(), i.toDouble(), i.toDouble())
+			
+			Assertions.assertThat(actual).isEqualTo(expected)
+		}
+		@Test
+		fun `using longs`() {
+			val rec = Circle(l, l, l)
+			val actual = rec.shape
+			val expected = Ellipse2D.Double(l.toDouble(), l.toDouble(), l.toDouble(), l.toDouble())
+			
+			Assertions.assertThat(actual).isEqualTo(expected)
+		}
+		@Test
+		fun `using floats`() {
+			val rec = Circle(f, f, f)
+			val actual = rec.shape
+			val expected = Ellipse2D.Double(f.toDouble(), f.toDouble(), f.toDouble(), f.toDouble())
+			
+			Assertions.assertThat(actual).isEqualTo(expected)
+		}
+		@Test
+		fun `using doubles`() {
+			val rec = Circle(d, d, d)
+			val actual = rec.shape
+			val expected = Ellipse2D.Double(d, d, d, d)
+			
+			Assertions.assertThat(actual).isEqualTo(expected)
+		}
+	}
+	
+	@Test
+	fun `circle cloning`() {
+		val expected = Circle(i, i, i)
+		val actual = expected.clone()
+		
+		Assertions.assertThat(actual).isEqualTo(expected)
+	}
+	
+}
