0d1d1707858f884ec27e9832f8789cac947f4894
marshallbrain <marshalldbrain@gmail.com>
2019-10-04 15:58:08 -0400

    Initial commit

---
+++ src/test/kotlin/com/brain/ion/components/vectors/CircleTest.kt
@@ -0,0 +1,5 @@
+package com.brain.ion.components.vectors
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class CircleTest
---
+++ src/test/kotlin/com/brain/ion/components/vectors/RectangleTest.kt
@@ -0,0 +1,12 @@
+package com.brain.ion.components.vectors
+
+import org.junit.jupiter.api.BeforeEach
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class RectangleTest {
+
+	@BeforeEach
+	fun setUp() {
+	}
+}
