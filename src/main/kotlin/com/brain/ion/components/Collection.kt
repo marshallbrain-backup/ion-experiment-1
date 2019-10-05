da4c6b900a3b9c81de42efaec5a1bc62f548a773
marshallbrain <marshalldbrain@gmail.com>
2019-10-05 12:51:32 -0400

    Implement collections of components

--- src/main/kotlin/com/brain/ion/components/Collection.kt
+++ src/main/kotlin/com/brain/ion/components/Collection.kt
@@ -1,4 +1,44 @@
 package com.brain.ion.components
 
-class Collection {
+import java.awt.geom.Area
+
+class Collection(vararg component: Component) : Component {
+	
+	private val components = mutableListOf(*component)
+	private val interactArea = Area()
+	
+	override var onClick: () -> Unit = {}
+	
+	override fun getComponents(): List<Component> {
+		return components.toList()
+	}
+	
+	fun addComponents(vararg component: Component) {
+		components.addAll(component)
+		interactArea.reset()
+	}
+	
+	fun removeComponents(vararg component: Component) {
+		components.addAll(component)
+		interactArea.reset()
+	}
+	
+	fun resetComponents() {
+		components.clear()
+		interactArea.reset()
+	}
+	
+	override fun getInteractArea(): Area {
+		if (interactArea.isEmpty) {
+			for (c in components) {
+				interactArea.add(c.getInteractArea())
+			}
+		}
+		return interactArea
+	}
+	
+	override fun clone(): Component {
+		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+	}
+	
 }
