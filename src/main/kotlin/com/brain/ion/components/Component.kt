2898481a91f3f0efe2669e1d29ad6dcf96e717c9
marshallbrain <marshalldbrain@gmail.com>
2019-10-05 10:51:54 -0400

    Add on click functionality
    This allows a custom function to be called when the component is click and provides a much higher level of combustibility.

--- src/main/kotlin/com/brain/ion/components/Component.kt
+++ src/main/kotlin/com/brain/ion/components/Component.kt
@@ -1,4 +1,28 @@
 package com.brain.ion.components
 
+import java.awt.Point
+import java.awt.geom.Area
+
 interface Component {
+	
+	var onClick: () -> Unit
+	
+	fun clicked(p: Point) {
+		if (isClicked(p)) {
+			onClick()
+		}
+	}
+	
+	fun getInteractArea() : Area {
+		return Area()
+	}
+	
+	fun isClicked(p: Point) : Boolean {
+		if (getInteractArea().contains(p)) {
+			return true
+		}
+		
+		return false
+	}
+
 }
