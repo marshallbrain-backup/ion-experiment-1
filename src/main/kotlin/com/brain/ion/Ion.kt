ea54bada71c771b0c516f113e5d02059336d1137
marshallbrain <marshalldbrain@gmail.com>
2019-10-05 23:03:38 -0400

    Add KDocs for the Ion class

--- src/main/kotlin/com/brain/ion/Ion.kt
+++ src/main/kotlin/com/brain/ion/Ion.kt
@@ -1,11 +1,25 @@
 package com.brain.ion
 
+/**
+ * The primary class for Ion
+ *
+ * @since 0.1
+ *
+ * @constructor Initializes and starts the game loop
+ * @param name The name of the main frame
+ * @since 0.1
+ */
 class Ion(name: String = "ion") {
 	
 	private val mainFrame = Frame(name = name, x = 50, y = 50, width = 1600, height = 900)
 	private val minorFrames = mutableListOf<Frame>()
 	private val gameLoop = GameLoop(mainFrame, minorFrames)
 	
+	/**
+	 * The graphics used to draw to the frames
+	 *
+	 * @since 0.1
+	 */
 	val ionGraphics = gameLoop.graphics
 	
 	init {
