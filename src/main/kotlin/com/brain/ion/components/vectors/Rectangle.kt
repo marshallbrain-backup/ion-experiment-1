16bc67e1b521776fdcd1ae30ed01f6c18910b33a
marshallbrain <marshalldbrain@gmail.com>
2019-10-16 23:34:36 -0400

    Add basic implementation

--- src/main/kotlin/com/brain/ion/components/vectors/Rectangle.kt
+++ src/main/kotlin/com/brain/ion/components/vectors/Rectangle.kt
@@ -1,4 +1,24 @@
 package com.brain.ion.components.vectors
 
-class Rectangle {
+import com.brain.ion.properties.Style
+import java.awt.Shape
+import java.awt.geom.Rectangle2D
+
+class Rectangle(
+		var x: Number,
+		var y: Number,
+		var width: Number,
+		var height: Number,
+		override val id: String,
+		override var style: Style = Style()
+) : Vector{
+	
+	override var shape: Shape = Rectangle2D.Double(
+			x.toDouble(), y.toDouble(), width.toDouble(), height.toDouble()
+	)
+	
+	override fun clone() {
+		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+	}
+	
 }
