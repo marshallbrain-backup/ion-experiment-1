06f0fc528fd33bb3ff9cbf768437235c7233d5f7
marshallbrain <marshalldbrain@gmail.com>
2019-10-17 12:50:30 -0400

    Add function to allow setting of vars with function

--- src/main/kotlin/com/brain/ion/components/Component.kt
+++ src/main/kotlin/com/brain/ion/components/Component.kt
@@ -1,11 +1,13 @@
 package com.brain.ion.components
 
+import com.brain.ion.properties.FunctionVariable
 import com.brain.ion.properties.Renderable
 
 interface Component{
 	
 	val id: String
 	
+	fun assign(varMap: MutableMap<String, FunctionVariable>)
 	fun clone()
 	
 }
