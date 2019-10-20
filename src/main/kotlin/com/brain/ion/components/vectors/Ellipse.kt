f6dad3541f06559c01bf7ed155ce789565cf6767
marshallbrain <marshalldbrain@gmail.com>
2019-10-20 12:53:23 -0400

    Add full functionality

--- src/main/kotlin/com/brain/ion/components/vectors/Ellipse.kt
+++ src/main/kotlin/com/brain/ion/components/vectors/Ellipse.kt
@@ -1,4 +1,60 @@
 package com.brain.ion.components.vectors
 
-class Ellipse {
+import com.brain.ion.components.Component
+import com.brain.ion.graphics.IonGraphics
+import java.awt.Shape
+import java.awt.geom.Ellipse2D
+
+class Ellipse(
+		override val id: String,
+		x: Number,
+		y: Number,
+		width: Number,
+		height: Number,
+		override var style: Style = Style()
+) : Vector{
+	
+	var x: Number = x
+		set(value) {
+			changed = true
+			field = value
+		}
+	var y: Number = y
+		set(value) {
+			changed = true
+			field = value
+		}
+	var width: Number = width
+		set(value) {
+			changed = true
+			field = value
+		}
+	var height: Number = height
+		set(value) {
+			changed = true
+			field = value
+		}
+	
+	override var onRender: (Component) -> Unit = emptyFunction
+	private var changed = false
+	private var shape: Ellipse2D =
+			Ellipse2D.Double(x.toDouble(), y.toDouble(), width.toDouble(), height.toDouble())
+	
+	override fun getShape(g: IonGraphics): Shape {
+		if (onRender != emptyFunction) {
+			onRender.invoke(this)
+			changed = true
+		}
+		
+		if(changed){
+			shape.setFrame(x.toDouble(), y.toDouble(), width.toDouble(), height.toDouble())
+		}
+		
+		return shape
+	}
+	
+	override fun clone() {
+		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+	}
+	
 }
