9e5fdd79edcdf591210d2e807ccce855685ac194
marshallbrain <marshalldbrain@gmail.com>
2019-10-21 22:37:27 -0400

    Finished

--- src/main/kotlin/com/brain/ion/components/Collection.kt
+++ src/main/kotlin/com/brain/ion/components/Collection.kt
@@ -1,4 +1,23 @@
 package com.brain.ion.components
 
-class Collection {
+class Collection(
+		override val id: String,
+		vararg components: Component,
+		componentList: MutableList<Component> = mutableListOf(*components)
+) : Component, MutableList<Component> by componentList{
+	
+	override var onRender: (Component) -> Unit = emptyFunction
+	
+	override fun clone() {
+		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+	}
+	
+	override fun getCollection(): List<Component> {
+		if (onRender != emptyFunction) {
+			onRender.invoke(this)
+		}
+		
+		return this.toList()
+	}
+	
 }
