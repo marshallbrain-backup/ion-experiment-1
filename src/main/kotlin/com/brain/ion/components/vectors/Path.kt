267c7e74c6da0d423bf4d0f9f9597e920aecae99
marshallbrain <marshalldbrain@gmail.com>
2019-10-23 10:36:20 -0400

    Function rework

--- src/main/kotlin/com/brain/ion/components/vectors/Path.kt
+++ src/main/kotlin/com/brain/ion/components/vectors/Path.kt
@@ -13,6 +13,10 @@ class Path(
 ) : Vector {
 	
 	companion object {
+		const val ADD: Int = 0
+		const val SET: Int = 1
+		const val REMOVE: Int = 2
+		
 		private fun constructPath(pathString: String): ShapeCustom {
 			
 			val pathList = pathString.trimEnd(';').split(";").map { it.trim() }
@@ -26,10 +30,10 @@ class Path(
 					'm' -> tempPath.move(par[0].toDouble(), par[1].toDouble())
 					'L' -> tempPath.lineTo(par[0].toDouble(), par[1].toDouble())
 					'l' -> tempPath.line(par[0].toDouble(), par[1].toDouble())
-					'X' -> tempPath.lineXTo(par[0].toDouble())
-					'x' -> tempPath.lineX(par[0].toDouble())
-					'Y' -> tempPath.lineYTo(par[0].toDouble())
-					'y' -> tempPath.lineY(par[0].toDouble())
+					'X' -> tempPath.lineTo(x = par[0].toDouble())
+					'x' -> tempPath.line(x = par[0].toDouble())
+					'Y' -> tempPath.lineTo(y = par[0].toDouble())
+					'y' -> tempPath.line(y = par[0].toDouble())
 					'z', 'Z' -> tempPath.close()
 				}
 			}
@@ -37,57 +41,41 @@ class Path(
 		}
 	}
 	
-	constructor(id: String, style: Style, pathString: String): this(id, style) {
-		shape.cloneCoordList(constructPath(pathString))
-	}
-	
-	val interactionShape = Path2D.Double()
 	private val shape = ShapeCustom()
 	
 	override var onRender: (Component) -> Unit = emptyFunction
+	
+	constructor(id: String, style: Style, pathString: String): this(id, style) {
+		shape.cloneCoordList(constructPath(pathString))
+	}
 	
 	override fun getShape(g: IonGraphics): Shape {
 		return shape
 	}
 	
-	fun moveTo(x: Number, y: Number, index: Int = -1, insert: Boolean = false) {
-		shape.moveTo(x.toDouble(), y.toDouble(), index, insert)
+	fun moveTo(x: Number, y: Number, type: Int = ADD, index: Int = shape.length){
+		shape.moveTo(x.toDouble(), y.toDouble(), type, index)
 	}
 	
-	fun move(x: Number, y: Number, index: Int = -1, insert: Boolean = false) {
+	fun move(x: Number, y: Number, type: Int = ADD, index: Int = shape.length){
 		val end = shape.currentPoint
-		shape.moveTo(end.x + x.toDouble(), end.y + y.toDouble(), index, insert)
+		shape.moveTo(end.x + x.toDouble(), end.y + y.toDouble(), type, index)
 	}
-	
-	fun lineTo(x: Number, y: Number, index: Int = -1, insert: Boolean = false) {
-		shape.lineTo(x.toDouble(), y.toDouble(), index, insert)
+
+	fun lineTo(
+			x: Number = shape.currentPoint.x, y: Number = shape.currentPoint.y,
+			type: Int = ADD, index: Int = shape.length
+	) {
+		shape.lineTo(x.toDouble(), y.toDouble(), type, index)
 	}
-	
-	fun line(x: Number, y: Number, index: Int = -1, insert: Boolean = false) {
+
+	fun line(
+			x: Number = 0, y: Number = 0, type: Int = ADD, index: Int = shape.length
+	) {
 		val end = shape.currentPoint
-		shape.lineTo(end.x + x.toDouble(), end.y + y.toDouble(), index, insert)
+		shape.lineTo(end.x + x.toDouble(), end.y + y.toDouble(), type, index)
 	}
-	
-	fun lineXTo(x: Number, index: Int = -1, insert: Boolean = false) {
-		val end = shape.currentPoint
-		shape.lineTo(x.toDouble(), end.y, index, insert)
-	}
-	
-	fun lineX(x: Number, index: Int = -1, insert: Boolean = false) {
-		val end = shape.currentPoint
-		shape.lineTo(end.x + x.toDouble(), end.y, index, insert)
-	}
-	
-	fun lineYTo(y: Number, index: Int = -1, insert: Boolean = false) {
-		val end = shape.currentPoint
-		shape.lineTo(end.x, y.toDouble(), index, insert)
-	}
-	
-	fun lineY(y: Number, index: Int = -1, insert: Boolean = false) {
-		val end = shape.currentPoint
-		shape.lineTo(end.x, end.y + y.toDouble(), index, insert)
-	}
-	
+
 	fun close(index: Int = -1, insert: Boolean = false) {
 		shape.close(index, insert)
 	}
@@ -106,6 +94,8 @@ class Path(
 		
 		private val coordList: LinkedList<CoordEntry> = LinkedList()
 		val currentPoint = Point2D.Double()
+		val length: Int
+			get() = coordList.size
 		
 		init {
 			coordList.add(CoordEntry(segMoveTo, doubleArrayOf(0.0, 0.0)))
@@ -116,49 +106,53 @@ class Path(
 			coordList.addAll(clone.coordList)
 		}
 		
-		fun moveTo(x: Double, y: Double, index: Int, insert: Boolean) {
+		fun moveTo(x: Double, y: Double, type: Int, index: Int) {
 			when {
 				coordList[coordList.size-1].type == segMoveTo -> {
 					coordList[coordList.size-1] = CoordEntry(segMoveTo, doubleArrayOf(x, y))
 				}
-				index != -1 -> coordList[index] = CoordEntry(segMoveTo, doubleArrayOf(x, y))
-				insert -> coordList.add(index, CoordEntry(segMoveTo, doubleArrayOf(x, y)))
-				else -> coordList.add(CoordEntry(segMoveTo, doubleArrayOf(x, y)))
+				type == 0 -> coordList.add(index, CoordEntry(segMoveTo, doubleArrayOf(x, y)))
+				type == 1 -> coordList[index] = CoordEntry(segMoveTo, doubleArrayOf(x, y))
+				type == 2 -> coordList.removeAt(index)
 			}
 			currentPoint.setLocation(x, y)
+			update = true
 		}
 		
-		fun lineTo(x: Double, y: Double, index: Int, insert: Boolean) {
-			when {
-				index != -1 -> coordList[index] = CoordEntry(segLineTo, doubleArrayOf(x, y))
-				insert -> coordList.add(index, CoordEntry(segLineTo, doubleArrayOf(x, y)))
-				else -> coordList.add(CoordEntry(segLineTo, doubleArrayOf(x, y)))
+		fun lineTo(x: Double, y: Double, type: Int, index: Int) {
+			when(type) {
+				0 -> coordList.add(index, CoordEntry(segLineTo, doubleArrayOf(x, y)))
+				1 -> coordList[index] = CoordEntry(segLineTo, doubleArrayOf(x, y))
+				2 -> coordList.removeAt(index)
 			}
 			currentPoint.setLocation(x, y)
+			update = true
 		}
 		
 		fun quadTo(x1: Double, y1: Double,
 		           x2: Double, y2: Double,
-		           index: Int, insert: Boolean
+		           type: Int, index: Int
 		) {
-			when {
-				index != -1 -> coordList[index] = CoordEntry(segQuadTo, doubleArrayOf(x1, y1, x2, y2))
-				insert -> coordList.add(index, CoordEntry(segQuadTo, doubleArrayOf(x1, y1, x2, y2)))
-				else -> coordList.add(CoordEntry(segQuadTo, doubleArrayOf(x1, y1, x2, y2)))
+			when(type) {
+				0 -> coordList.add(index, CoordEntry(segQuadTo, doubleArrayOf(x1, y1, x2, y2)))
+				1 -> coordList[index] = CoordEntry(segQuadTo, doubleArrayOf(x1, y1, x2, y2))
+				2 -> coordList.removeAt(index)
 			}
+			update = true
 		}
 		
 		fun curveTo(
 				x1: Double, y1: Double,
 				x2: Double, y2: Double,
 				x3: Double, y3: Double,
-				index: Int, insert: Boolean
+				type: Int, index: Int
 		) {
-			when {
-				index != -1 -> coordList[index] = CoordEntry(segCubicTo, doubleArrayOf(x1, y1, x2, y2, x3, y3))
-				insert -> coordList.add(index, CoordEntry(segCubicTo, doubleArrayOf(x1, y1, x2, y2, x3, y3)))
-				else -> coordList.add(CoordEntry(segCubicTo, doubleArrayOf(x1, y1, x2, y2, x3, y3)))
+			when(type) {
+				0 -> coordList.add(index, CoordEntry(segCubicTo, doubleArrayOf(x1, y1, x2, y2, x3, y3)))
+				1 -> coordList[index] = CoordEntry(segCubicTo, doubleArrayOf(x1, y1, x2, y2, x3, y3))
+				2 -> coordList.removeAt(index)
 			}
+			update = true
 		}
 		
 		fun close(index: Int, insert: Boolean){
@@ -167,10 +161,15 @@ class Path(
 				insert -> coordList.add(index, CoordEntry(segClose, doubleArrayOf()))
 				else -> coordList.add(CoordEntry(segClose, doubleArrayOf()))
 			}
+			update = true
 		}
 		
 		override fun getPathIterator(at: AffineTransform?): PathIterator {
-			return PathIteratorCustom(coordList.toList())
+			return PathIteratorCustom(coordList.toList(), at)
+		}
+		
+		fun clone() {
+			TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 		}
 		
 		class CoordEntry(
@@ -181,7 +180,8 @@ class Path(
 	}
 	
 	private class PathIteratorCustom(
-			private val coordList: List<ShapeCustom.CoordEntry>
+			private val coordList: List<ShapeCustom.CoordEntry>,
+			private val at: AffineTransform?
 	): PathIterator{
 		
 		private val curvecoords = intArrayOf(2, 2, 4, 6, 0)
@@ -200,11 +200,12 @@ class Path(
 			val type = coordList[typeIdx].type.toInt()
 			val numCoords = curvecoords[type]
 			if (numCoords > 0) {
-				for (i in 0 until numCoords) {
-					coords[i] = coordList[typeIdx].coords[i].toFloat()
-					
-//					affine.transform(doubleCoords, pointIdx,
-//							coords, 0, numCoords / 2)
+				if(at == null){
+					for (i in 0 until numCoords) {
+						coords[i] = coordList[typeIdx].coords[i].toFloat()
+					}
+				} else {
+					at.transform(coordList[typeIdx].coords, 0, coords, 0, numCoords / 2)
 				}
 			}
 			return type
@@ -214,8 +215,12 @@ class Path(
 			val type = coordList[typeIdx].type.toInt()
 			val numCoords = curvecoords[type]
 			if (numCoords > 0) {
-				for (i in 0 until numCoords) {
-					coords[i] = coordList[typeIdx].coords[i]
+				if(at == null){
+					for (i in 0 until numCoords) {
+						coords[i] = coordList[typeIdx].coords[i]
+					}
+				} else {
+					at.transform(coordList[typeIdx].coords, 0, coords, 0, numCoords / 2)
 				}
 			}
 			return type
@@ -229,6 +234,17 @@ class Path(
 	
 	private open class ShapeImpl(): Shape{
 		
+		private val path: Path2D = Path2D.Double()
+		
+		protected var update: Boolean = false
+		
+		private fun update(){
+			if (update){
+				path.reset()
+				path.append(this, false)
+			}
+		}
+		
 		override fun getPathIterator(at: AffineTransform?): PathIterator {
 			TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 		}
@@ -238,35 +254,43 @@ class Path(
 		}
 		
 		override fun contains(x: Double, y: Double): Boolean {
-			TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+			update()
+			return path.contains(x, y)
 		}
 		
 		override fun contains(p: Point2D?): Boolean {
-			TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+			update()
+			return path.contains(p)
 		}
 		
 		override fun contains(x: Double, y: Double, w: Double, h: Double): Boolean {
-			TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+			update()
+			return path.contains(x, y, w, h)
 		}
 		
 		override fun contains(r: Rectangle2D?): Boolean {
-			TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+			update()
+			return path.contains(r)
 		}
 		
 		override fun getBounds2D(): Rectangle2D {
-			TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+			update()
+			return path.bounds2D
 		}
 		
 		override fun intersects(x: Double, y: Double, w: Double, h: Double): Boolean {
-			TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+			update()
+			return path.intersects(x, y, w, h)
 		}
 		
 		override fun intersects(r: Rectangle2D?): Boolean {
-			TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+			update()
+			return path.intersects(r)
 		}
 		
 		override fun getBounds(): Rectangle {
-			TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+			update()
+			return path.bounds
 		}
 		
 	}
