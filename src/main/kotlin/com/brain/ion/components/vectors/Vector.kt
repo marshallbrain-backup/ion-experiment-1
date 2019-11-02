9a5819d80cecc0b050de60ac5a90976578b4fbbd
marshallbrain <marshalldbrain@gmail.com>
2019-11-02 14:03:42 -0400

    Implement Clickable in Vector

--- src/main/kotlin/com/brain/ion/components/vectors/Vector.kt
+++ src/main/kotlin/com/brain/ion/components/vectors/Vector.kt
@@ -2,15 +2,20 @@ package com.brain.ion.components.vectors
 
 import com.brain.ion.components.Component
 import com.brain.ion.graphics.IonGraphics
+import com.brain.ion.properties.Clickable
 import java.awt.Shape
+import java.awt.geom.Area
 
-interface Vector : Component{
+interface Vector : Component, Clickable{
+	
+	override val area: Area
+		get() = Area(getShape())
 	
 	var style: Style
 	
-	fun getShape(g: IonGraphics = IonGraphics()): Shape
+	fun getShape(): Shape
 	
-	override fun getCollection(): List<Component> {
+	override fun getCollection(g: IonGraphics): List<Component> {
 		TODO("not implemented")
 	}
 	
--- src/main/kotlin/com/brain/ion/properties/Clickable.kt
+++ src/main/kotlin/com/brain/ion/properties/Clickable.kt
@@ -1,4 +1,11 @@
 package com.brain.ion.properties
 
+import java.awt.geom.Area
+import java.awt.geom.Point2D
+
 interface Clickable {
+	
+	val area: Area
+	var onClick: (Point2D) -> Unit
+	
 }
