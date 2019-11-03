3f5f199d09df95c9465943709d8e1ab467fd2b01
marshallbrain <marshalldbrain@gmail.com>
2019-11-02 23:51:32 -0400

    Implement base version

--- src/main/kotlin/com/brain/ion/components/Text.kt
+++ src/main/kotlin/com/brain/ion/components/Text.kt
@@ -1,4 +1,41 @@
 package com.brain.ion.components
 
-class Text {
+import com.brain.ion.components.vectors.Path
+import com.brain.ion.components.vectors.Style
+import com.brain.ion.graphics.IonGraphics
+import java.awt.Font
+
+class Text(
+		override val id: String,
+		t: String = "",
+		override var x: Number = 0,
+		override var y: Number = 0,
+		var style: Style = Style()
+) : Component {
+	
+	override var onRender: (Component) -> Unit = emptyFunction
+	private var changed = false
+	var text = t.trim()
+		set(value) {
+			changed = true
+			field = value
+		}
+	
+	override fun clone(): Component {
+		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+	}
+	
+	override fun getCollection(graphics: IonGraphics): List<Component> {
+		
+		val g = graphics.graphics
+		val font = Font("Open Sans", Font.PLAIN, 18)
+		val fontMetrics = g.getFontMetrics(font)
+		val glyph = font.createGlyphVector(fontMetrics.fontRenderContext, text)
+		
+		val outline = glyph.getOutline(0f, -glyph.visualBounds.y.toFloat())
+		
+		return listOf(Path("id", style, outline))
+		
+	}
+	
 }
