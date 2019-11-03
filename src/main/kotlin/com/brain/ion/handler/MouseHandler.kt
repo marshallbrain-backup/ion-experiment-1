5cf09c9a50c8c79d7e3341619d235d92bd030331
marshallbrain <marshalldbrain@gmail.com>
2019-11-03 10:33:15 -0500

    Add support for collections

--- src/main/kotlin/com/brain/ion/handler/MouseHandler.kt
+++ src/main/kotlin/com/brain/ion/handler/MouseHandler.kt
@@ -1,23 +1,45 @@
 package com.brain.ion.handler
 
+import com.brain.ion.components.Collection
+import com.brain.ion.components.Component
 import com.brain.ion.graphics.Group
 import com.brain.ion.properties.Clickable
 import java.awt.event.MouseEvent
 import java.awt.event.MouseListener
+import java.awt.geom.Point2D
 
 class MouseHandler(private val renderQueue: Group) : MouseListener {
 	
-	private fun iterateGroup(group: Group, e: MouseEvent) {
+	private fun iterateGroup(group: List<*>, p: Point2D, e: MouseEvent) : Boolean {
+		
 		for (c in group) {
-			if (c is Group) iterateGroup(c, e)
-			else if (c is Clickable && c.onClick != {} && c.area.contains(e.point)) {
-				c.onClick.invoke(e.point)
+			if (c is Group) {
+				if (iterateGroup(c, p, e)) return true
+			} else if (c is Collection) {
+				if (iterateGroup(c, Point2D.Double(p.x - c.x.toDouble(), p.y - c.y.toDouble()), e)) return true
+			} else if (c is Clickable) {
+				
+				if (c.onClick != c.clickEmpty) {
+					if (c.area.contains(p)){
+						if ((c as Component).visible) {
+							
+							c.onClick.invoke(p)
+							return true
+							
+						}
+					}
+				}
+				
 			}
+			
 		}
+		
+		return false
+		
 	}
 	
 	override fun mouseClicked(e: MouseEvent) {
-		iterateGroup(renderQueue, e)
+		iterateGroup(renderQueue, e.point, e)
 	}
 	
 	override fun mousePressed(p0: MouseEvent?) {
