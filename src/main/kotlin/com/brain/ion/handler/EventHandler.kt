d964d129daf7267503238928718df0d26b0f0abb
marshallbrain <marshalldbrain@gmail.com>
2019-11-04 07:30:02 -0500

    Rename Event to EventType

--- src/main/kotlin/com/brain/ion/handler/EventHandler.kt
+++ src/main/kotlin/com/brain/ion/handler/EventHandler.kt
@@ -1,15 +1,15 @@
 package com.brain.ion.handler
 
 class EventHandler(
-		private val map: MutableMap<Event, MutableSet<(Any) -> Unit>> = mutableMapOf()
-) : Map<Event, MutableSet<(Any) -> Unit>> by map {
+		private val map: MutableMap<EventType, MutableSet<(Any) -> Unit>> = mutableMapOf()
+) : Map<EventType, MutableSet<(Any) -> Unit>> by map {
 	
-	fun add(target: Event, trigger: (Any) -> Unit) {
+	fun add(target: EventType, trigger: (Any) -> Unit) {
 		val list = map.getOrPut(target) { mutableSetOf() }
 		list.add(trigger)
 	}
 	
-	fun trigger(source: Any, target: Event) {
+	fun trigger(source: Any, target: EventType) {
 		get(target)?.forEach { it.invoke(source) }
 	}
 	
